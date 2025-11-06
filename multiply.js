89da12c4914a0e93c08137a65249dcc2d4bd5a0e
sebastian <sebastianraigoso.dev@gmail.com>
2025-11-06 18:36:09 +0900

    codewars exo

---
+++ multiply.js
@@ -0,0 +1,13 @@
+// This code does not execute properly. Try to figure out why.
+
+function multiply(a, b){
+  return a * b 
+}
+
+// they forgot to return 
+
+console.log(multiply(1,1)) // 1
+console.log(multiply(2,1)) // 2
+console.log(multiply(0,0)) // 0
+console.log(multiply(2,2)) // 4
+console.log(multiply(3,5)) // 15
