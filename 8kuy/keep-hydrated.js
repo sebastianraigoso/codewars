5cc9358056a1330f627e2070f8bc78e0c4c65ffa
sebastian <sebastianraigoso.dev@gmail.com>
2025-11-07 13:26:28 +0900

    codewars exo

---
+++ 8kuy/keep-hydrated.js
@@ -0,0 +1,23 @@
+// Nathan loves cycling.
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded down.
+
+// For example:
+// time = 3 ----> litres = 1
+// time = 6.7---> litres = 3
+// time = 11.8--> litres = 5
+
+function litres(time) {
+    return Math.floor(time * 0.5)
+}
+
+console.log(litres(0)) // 0
+console.log(litres(1)) // 0
+console.log(litres(2)) // 1
+console.log(litres(3)) // 1
+console.log(litres(4)) // 2
+console.log(litres(1.4)) // 0
+console.log(litres(12.3)) // 6
+console.log(litres(0.82)) // 0
+console.log(litres(11.8)) // 5
+console.log(litres(1787)) // 893
