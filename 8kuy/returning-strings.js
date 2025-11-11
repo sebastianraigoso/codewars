630393c66652ac7ce0f37b09aaba2b878f9dd631
sebastian <sebastianraigoso.dev@gmail.com>
2025-11-12 07:29:36 +0900

    codewars exo

---
+++ 8kuy/returning-strings.js
@@ -0,0 +1,11 @@
+// Create a function that accepts a parameter representing a name and returns the message:
+// "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+function greet(name) {
+    return `Hello, ${name} how are you doing today?`
+}
+
+console.log(greet("Ryan")) // "Hello, Ryan how are you doing today?"
+console.log(greet("Shingles")) // "Hello, Shingles how are you doing today?"
