cffa4f305dcc432ab1a41e2b01f63304e3babe92
sebastian <sebastianraigoso.dev@gmail.com>
2025-11-11 16:16:28 +0900

    codewars exo

---
+++ 8kuy/string-repeat.js
@@ -0,0 +1,18 @@
+// Write a function that accepts a non-negative integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+function repeatStr (n, s) {
+    return n >= 0 ? s.repeat(n) : 'negative integer'
+}
+
+console.log(repeatStr(3, "*")) // "***"
+console.log(repeatStr(5, "#")) // "#####"
+console.log(repeatStr(2, "ha ")) // "ha ha "
+console.log(repeatStr(0, "")) // ""
+console.log(repeatStr(0, "I")) // ""
+console.log(repeatStr(5, "")) // ""
+console.log(repeatStr(6, "I")) // "IIIIII"
+console.log(repeatStr(5, "Hello")) // "HelloHelloHelloHelloHello"
