b79681e16ab4f81c89fa01ea23d92dd132a8c4fc
sebastian <sebastianraigoso.dev@gmail.com>
2025-11-25 19:09:57 +0900

    codewars exo

---
+++ 6kuy/stop-gninnipS-My-sdroW.js
@@ -0,0 +1,23 @@
+// Write a function that takes in a string of one or more words, and returns the same string, but with all words that have five or more letters reversed (just like the name of this kata). 
+// Strings passed in will consist of only letters and spaces. 
+// Spaces will be included only when more than one word is present.
+
+// Examples:
+
+// "Hey fellow warriors"  --> "Hey wollef sroirraw" 
+// "This is a test        --> "This is a test" 
+// "This is another test" --> "This is rehtona test"
+
+function spinWords(string) {
+    const strSplit = string.split(' ')
+    return strSplit.map(e => e.length >= 5 ? e.split('').reverse().join('') : e).join(' ')
+
+}
+
+console.log(spinWords("Welcome")) // "emocleW"
+console.log(spinWords("Hey fellow warriors")) // "Hey wollef sroirraw"
+console.log(spinWords("This is a test")) // "This is a test"
+console.log(spinWords("This is another test")) // "This is rehtona test"
+console.log(spinWords("You are almost to the last test")) // "You are tsomla to the last test"
+console.log(spinWords("Just kidding there is still one more")) // "Just gniddik ereht is llits one more"
+console.log(spinWords("Seriously this is the last one")) // "ylsuoireS this is the last one"
