82c6983a4e0fb583ffcc1df6fa76b17155044229
sebastian <sebastianraigoso.dev@gmail.com>
2025-12-02 17:05:51 +0900

    codewars exo

--- 7kuy/credit-card-mask.js
+++ 7kuy/credit-card-mask.js
@@ -16,5 +16,9 @@
 
 // return masked string
 function maskify(cc) {
-    return cc.length > 4 ? cc.slice(0, -4).replaceAll('#') + cc.slice(-4) : cc
+    return cc.length > 4 ? cc.slice(cc.length-4).padStart(cc.length, '#') : cc
 }
+
+console.log(maskify('4556364607935616')) // '############5616'
+console.log(maskify('1')) // '1'
+console.log(maskify('11111')) // '#1111'
