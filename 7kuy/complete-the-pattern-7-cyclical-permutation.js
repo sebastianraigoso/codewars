91e53f3b4bd8374094d1b60a87b750ff6d9e31dd
sebastian <sebastianraigoso.dev@gmail.com>
2025-12-04 04:46:23 +0900

    codewars exo

---
+++ 7kuy/complete-the-pattern-7-cyclical-permutation.js
@@ -0,0 +1,54 @@
+// You have to write a function which creates the following pattern (See Examples) upto desired number of rows.
+
+// If the Argument is 0 or a Negative Integer then it should return "" i.e. empty string.
+
+// Examples:
+// Argument: 9
+
+// 123456789
+// 234567891
+// 345678912
+// 456789123
+// 567891234
+// 678912345
+// 789123456
+// 891234567
+// 912345678
+
+// Argument: 11
+
+// 1234567891011
+// 2345678910111
+// 3456789101112
+// 4567891011123
+// 5678910111234
+// 6789101112345
+// 7891011123456
+// 8910111234567
+// 9101112345678
+// 1011123456789
+// 1112345678910
+
+// Note: There are no spaces in the pattern
+// Hint: Use \n in string to jump to next line
+
+function pattern(n) {
+    if(n > 0) {
+        let nums = []
+        for(let i = 1; i <= n; i++) {
+            nums.push(i)
+        }
+
+        let result = []
+        for(let j = 0; j < n; j++) {
+            result.push(nums.slice(j).concat(nums.slice(0, j)))
+        }
+        return result.map(e => e.join('')).join('\n')
+    }else {
+        return ""
+    }
+}
+
+console.log(pattern(11))
+console.log(pattern(0))
+console.log(pattern(-2))
