677e6b33c0d07a50d588b8551701747adfd697f4
sebastian <sebastianraigoso.dev@gmail.com>
2025-12-05 07:56:42 +0900

    codewars exo

--- 5kuy/human-readable-time.js
+++ 5kuy/human-readable-time.js
@@ -12,7 +12,7 @@ function humanReadable (seconds) {
     const minutes = Math.floor((seconds % 3600) / 60)
     const secs = seconds % 60
 
-    return [String(hours).padStart(2, '0'), String(minutes).padStart(2, '0'), String(sec).padStart(2, '0'),].join(';')
+    return [String(hours).padStart(2, '0'), String(minutes).padStart(2, '0'), String(secs).padStart(2, '0'),].join(';')
 }
 
 
