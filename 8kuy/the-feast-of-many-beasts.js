35f589462d735b9f3a67dfa9332dd4c774a57436
sebastian <sebastianraigoso.dev@gmail.com>
2025-12-05 06:35:34 +0900

    codewars new solution

--- 8kuy/the-feast-of-many-beasts.js
+++ 8kuy/the-feast-of-many-beasts.js
@@ -11,6 +11,10 @@ function feast(beast, dish) {
     return beast.charAt(0) === dish.charAt(0) && beast.charAt(beast.length-1) === dish.charAt(dish.length-1)
 }
 
+function feast(beast, dish) {
+    return dish.startsWith(beast[0]) && dish.endsWith(beast[beast.length-1])
+}
+
 console.log(feast("great blue heron", "garlic naan")) //  true
 console.log(feast("chickadee", "chocolate cake")) //  true
 console.log(feast("brown bear", "bear claw")) // false
