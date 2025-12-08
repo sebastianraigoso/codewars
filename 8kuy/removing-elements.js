bc7f10e08863d29a3992079b84202ca6f35d3ad0
sebastian <sebastianraigoso.dev@gmail.com>
2025-12-09 06:58:59 +0900

    codewars exo

--- 8kuy/removing-elements.js
+++ 8kuy/removing-elements.js
@@ -6,6 +6,7 @@
 
 // None of the arrays will be empty, so you don't have to worry about that!
 
+// Solution 1
 function removeEveryOther(arr) {
     let remove = []
     for(let i = 0; i < arr.length; i++) {
@@ -15,3 +16,15 @@ function removeEveryOther(arr) {
     }
     return remove
 }
+
+// Solution 2
+function removeEveryOther(arr){
+    return arr.filter((_, index) => {
+        return index % 2 === 0
+    })
+}
+
+console.log(removeEveryOther(['Hello', 'Goodbye', 'Hello Again'])) // ['Hello', 'Hello Again']
+console.log(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])) // [1, 3, 5, 7, 9]
+console.log(removeEveryOther([[1, 2]])) //  [[1, 2]]
+console.log(removeEveryOther([['Goodbye'], {'Great': 'Job'}])) // [['Goodbye']]
