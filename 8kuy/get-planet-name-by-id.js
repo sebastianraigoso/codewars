caba9e6f13b8e186413149f57c309477267e81f8
sebastian <sebastianraigoso.dev@gmail.com>
2025-12-16 13:27:25 +0900

    codewars exo

---
+++ 8kuy/get-planet-name-by-id.js
@@ -0,0 +1,58 @@
+// The function is not returning the correct values. Can you figure out why?
+
+// Example (Input --> Output ):
+// 3 --> "Earth"
+
+// Solution 1
+function getPlanetName(id){
+  switch(id){
+    case 1:
+      return 'Mercury'
+    case 2:
+      return 'Venus'
+    case 3:
+      return 'Earth'
+    case 4:
+      return 'Mars'
+    case 5:
+      return 'Jupiter'
+    case 6:
+      return 'Saturn'
+    case 7:
+      return 'Uranus'
+    case 8:
+      return 'Neptune'
+  }
+}
+
+// Solution 2
+function getPlanetName(id){
+  var name;
+  switch(id){
+    case 1:
+      name = 'Mercury'
+      break;
+    case 2:
+      name = 'Venus'
+      break;
+    case 3:
+      name = 'Earth'
+      break;
+    case 4:
+      name = 'Mars'
+      break;
+    case 5:
+      name = 'Jupiter'
+      break;
+    case 6:
+      name = 'Saturn'
+      break;
+    case 7:
+      name = 'Uranus'
+      break;
+    case 8:
+      name = 'Neptune'
+  }
+  
+  return name;
+}
