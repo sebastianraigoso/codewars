c5c4a68a9f2afc41a597b3ded45a66e79bc055e5
sebastian <sebastianraigoso.dev@gmail.com>
2025-12-19 06:50:59 +0900

    codewars exo

--- 7kuy/sort-number.js
+++ 7kuy/sort-number.js
@@ -2,6 +2,9 @@
 // If the function passes in an empty array or null/nil value then it should return an empty array.
 
 // For example:
-
 // solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
 // solution(null); // should return []
+
+function solution(nums){
+    return nums ? nums.sort((a,b) => a - b): []
+}
