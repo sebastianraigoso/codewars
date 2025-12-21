59d314b5424aeba26b362eaabe3fdc53f38ec411
sebastian <sebastianraigoso.dev@gmail.com>
2025-12-21 17:59:02 +0900

    codewars exo

---
+++ 7kuy/anagram-detection.js
@@ -0,0 +1,48 @@
+// An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+// Note: anagrams are case insensitive
+
+// Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+// Examples
+// "foefet" is an anagram of "toffee"
+
+// "Buckethead" is an anagram of "DeathCubeK"
+
+// write the function isAnagram
+
+const isAnagram = function(test, original) {
+    test = test.toLowerCase()
+    original = original.toLowerCase()
+    let testList = {}
+    let originalList = {}
+
+    for(let characters in test) {
+        if(testList.hasOwnProperty(test[characters])) {
+            testList[test[characters]] += 1
+        } else {
+            testList[test[characters]] = 1
+        }
+    }
+
+    for(let characters in original) {
+        if(originalList.hasOwnProperty(original[characters])) {
+            originalList[original[characters]] += 1
+        } else {
+            originalList[original[characters]] = 1
+        }
+    }
+
+    return test.length === original.length &&
+       Object.keys(testList).length === Object.keys(originalList).length &&
+       Object.keys(testList).every(
+         key => testList[key] === originalList[key]
+    )
+}
+
+console.log(isAnagram("foefet", "toffee")) // true
+console.log(isAnagram("Buckethead", "DeathCubeK")) // true
+console.log(isAnagram("Twoo", "WooT")) // true
+console.log(isAnagram("dumble", "bumble")) // false
+console.log(isAnagram("ound", "round")) // false
+console.log(isAnagram("apple", "pale")) // false
