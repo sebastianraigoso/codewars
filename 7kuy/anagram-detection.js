4ba25988ed4f4d8ba1c24fdf899376b4cca16e18
sebastian <sebastianraigoso.dev@gmail.com>
2025-12-21 17:59:24 +0900

    codewarse exo

--- 7kuy/anagram-detection.js
+++ 7kuy/anagram-detection.js
@@ -11,6 +11,8 @@
 
 // write the function isAnagram
 
+
+// Solution 1
 const isAnagram = function(test, original) {
     test = test.toLowerCase()
     original = original.toLowerCase()
@@ -34,11 +36,17 @@ const isAnagram = function(test, original) {
     }
 
     return test.length === original.length &&
-       Object.keys(testList).length === Object.keys(originalList).length &&
-       Object.keys(testList).every(
-         key => testList[key] === originalList[key]
+        Object.keys(testList).length === Object.keys(originalList).length &&
+        Object.keys(testList).every(
+            key => testList[key] === originalList[key]
     )
 }
+
+// Solution 2
+function isAnagram (test, original) {
+	return test.toLowerCase().split("").sort().join("") === original.toLowerCase().split("").sort().join("")
+}
+
 
 console.log(isAnagram("foefet", "toffee")) // true
 console.log(isAnagram("Buckethead", "DeathCubeK")) // true
