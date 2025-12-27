6bbc84c70edd07d22c90d8520a1f8860e2e8fb36
sebastian <sebastianraigoso.dev@gmail.com>
2025-12-27 11:37:04 +0900

    codewars exo

---
+++ 7kuy/check-the-exam.js
@@ -0,0 +1,34 @@
+// The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. 
+// The second one contains a student's submitted answers.
+
+// The two arrays are not empty and are the same length. 
+// Return the score for this array of answers, 
+    // giving +4 for each correct answer, 
+    // -1 for each incorrect answer, 
+    // and +0 for each blank answer.
+
+// If the score < 0, return 0.
+
+// For example:
+
+//     Correct answer    |    Student's answer   |   Result         
+//  ---------------------|-----------------------|-----------
+//  ["a", "a", "b", "b"]   ["a", "c", "b", "d"]  →     6
+//  ["a", "a", "c", "b"]   ["a", "a", "b", "" ]  →     7
+//  ["a", "a", "b", "c"]   ["a", "a", "b", "c"]  →     16
+//  ["b", "c", "b", "a"]   ["" , "a", "a", "c"]  →     0
+
+function checkExam(array1, array2) {
+  let result = 0
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array2[i] === "") {
+      result += 0
+    } else if (array1[i] === array2[i]) {
+      result += 4
+    } else {
+      result -= 1
+    }
+  }
+  return result < 0 ? 0 : result
+}
