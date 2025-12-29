cf2ddcec4ebbc3615f9ef9bf4fed057b0233bce6
sebastian <sebastianraigoso.dev@gmail.com>
2025-12-29 10:19:27 +0900

    codewars exo

--- 7kuy/sum-of-minimums.js
+++ 7kuy/sum-of-minimums.js
@@ -11,7 +11,7 @@
 // Note: You will always be given a non-empty list containing positive values.
 
 function sumOfMinimums(arr) {
-    
+    return arr.map(e => Math.min(...e)).reduce((a,c) => a + c, 0)
 }
 
 console.log(sumOfMinimums([[11, 12, 14, 54], [67, 89, 90, 56], [7, 9, 4, 3], [9, 8, 6, 7]])) // 76
