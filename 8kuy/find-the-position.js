1f033c3c34eb4f3a75018534d527d72968af3d69
sebastian <sebastianraigoso.dev@gmail.com>
2026-01-09 11:34:05 +0900

    codewars exo

--- 8kuy/find-the-position.js
+++ 8kuy/find-the-position.js
@@ -5,3 +5,12 @@
 
 // Note: Only lowercased English letters are tested
 
+function position(letter) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    const position = alphabet.indexOf(letter)+1
+    return alphabet.includes(letter) ? `Position of alphabet: ${position}` : alphabet
+}
+
+console.log(position("a")) // "Position of alphabet: 1"
+console.log(position("z")) // "Position of alphabet: 26"
+console.log(position("e")) // "Position of alphabet: 5"
