20d241aa743ebbfa35e7870c51c0a007f36fbdf4
sebastian <sebastianraigoso.dev@gmail.com>
2026-01-18 18:06:57 +0900

    codewars exo

---
+++ 8kuy/capitalization-and-mutability.js
@@ -0,0 +1,23 @@
+// Your coworker was supposed to write a simple helper function to capitalize a string (that contains a single word) before they went on vacation.
+
+// Unfortunately, they have now left and the code they gave you doesn't work. 
+// Fix the helper function they wrote so that it works as intended (i.e. it must make the first character in the string upper case).
+
+// The string will always start with a letter and will never be empty.
+
+// Examples:
+// "hello" --> "Hello"
+// "Hello" --> "Hello" (the first letter was already capitalized)
+// "a"     --> "A"
+
+// Solution 1
+function capitalizeWord(word) {
+    return word[0].toUpperCase() + word.slice(1)
+}
+
+// Solution 2
+const capitalizeWord = (word) => word.replace(word.charAt(0), word.charAt(0).toUpperCase());
+
+console.log(capitalizeWord('word')) // 'Word'
+console.log(capitalizeWord('i')) // 'I'
+console.log(capitalizeWord('glasswear')) // 'Glasswear'
