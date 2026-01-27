448a83a7f98365a654200c8d611cd14d30ff0082
sebastian <sebastianraigoso.dev@gmail.com>
2026-01-27 16:51:32 +0900

    new solution

--- 7kuy/maximum-product.js
+++ 7kuy/maximum-product.js
@@ -4,12 +4,22 @@
 // Examples
 // [1, 2, 3] returns 6 because the maximum product is obtained from multiplying
 
+// Solution 1
 function adjacentElementsProduct(array) {
     let max = -Infinity
     for(let i = 0; i < array.length-1; i++) {
         if(max < array[i] * array[i+1]) {
             max = array[i] * array[i+1]
         }
+    }
+    return max
+}
+
+// Solution 2
+const adjacentElementsProduct = (array) => {
+    let max = array[0] * array[1]
+    for(let i = 1; i < array.length - 1; i++) {
+        max = Math.max(max, array[i] * array[i+1])
     }
     return max
 }
