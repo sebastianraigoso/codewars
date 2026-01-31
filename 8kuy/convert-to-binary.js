d67974be4f6ad5a414af4706ad6ec19a295da798
sebastian <sebastianraigoso.dev@gmail.com>
2026-02-01 07:43:40 +0900

    codewars

---
+++ 8kuy/convert-to-binary.js
@@ -0,0 +1,16 @@
+// Given a non-negative integer b, write a function which returns an integer d such that the binary representation of b
+// is the same as the decimal representation of d.
+
+// Examples:
+// n = 1 should return 1
+// n = 5 should return 101
+// n = 11 should return 1011
+
+function toBinary(n) {
+    return +n.toString(2)
+}
+
+console.log(toBinary(1)) // 1
+console.log(toBinary(2)) // 10
+console.log(toBinary(3)) // 11
+console.log(toBinary(5)) // 101
