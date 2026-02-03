8e91f062c11449c5782a976c44cba1a2e1295e1c
sebastian <sebastianraigoso.dev@gmail.com>
2026-02-03 17:39:31 +0900

    other solution

--- 8kuy/enumerable-magic-does-my-list-include-this.js
+++ 8kuy/enumerable-magic-does-my-list-include-this.js
@@ -1,8 +1,14 @@
 // Create a method that accepts a list and an item, and returns true if the item belongs to the list, 
 // otherwise false.
 
+// Solution 1
 function include(arr, item) {
     return arr.includes(item)
+}
+
+// Solution 2
+function include(arr, item) {
+    return arr.indexOf(item) !== -1
 }
 
 console.log(include([1,2,3,4], 3)) // true 
