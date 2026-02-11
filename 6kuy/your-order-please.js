01ff93ff9d57a24ba8df3e2dd0f7d877b6978415
sebastian <sebastianraigoso.dev@gmail.com>
2026-02-12 06:28:12 +0900

    codewars

---
+++ 6kuy/your-order-please.js
@@ -0,0 +1,29 @@
+// Your task is to sort a given string. 
+// Each word in the string will contain a single number. 
+// This number is the position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. 
+// So 1 will be the first word (not 0).
+
+// If the input string is empty, return an empty string. 
+// The words in the input String will only contain valid consecutive numbers.
+
+// Examples
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+
+function order(words) {
+    words = words.split(' ')
+    const count = words.length
+    let newOrder = []
+
+    for(let i = 1; i <= count; i++) {
+        newOrder.push(words.find(e => e.includes(String(i))))
+    }
+    return newOrder.join(' ')
+}
+
+console.log(order("is2 Thi1s T4est 3a")) // "Thi1s is2 3a T4est"
+console.log(order("4of Fo1r pe6ople g3ood th5e the2")) // "Fo1r the2 g3ood 4of th5e pe6ople"
+console.log(order("")) // ""
--- 7kuy/minimize-sum-of-array.js
+++ 7kuy/minimize-sum-of-array.js
@@ -33,7 +33,6 @@ function minSum(arr) {
     return count
 }
 
-
 console.log(minSum([5,4,2,3])) // 22
 console.log(minSum([12,6,10,26,3,24])) // 342
 console.log(minSum([9,2,8,7,5,4,0,6])) // 74
