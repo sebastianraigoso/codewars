7f0ec82bfbfa6d0df42d382383678cc44d3ed656
sebastian <sebastianraigoso.dev@gmail.com>
2026-02-11 22:25:40 +0900

    codewars

---
+++ 7kuy/alphabet-war.js
@@ -0,0 +1,61 @@
+// There is a war and nobody knows - the alphabet war!
+// There are two groups of hostile letters.
+// The tension between left side letters and right side letters was too high and the war began.
+
+// Task
+// Write a function that accepts fight string consists of only small letters and return who wins the fight.
+// When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
+
+// The left side letters and their power:
+//  w - 4
+//  p - 3
+//  b - 2
+//  s - 1
+
+// The right side letters and their power:
+//  m - 4
+//  q - 3
+//  d - 2
+//  z - 1
+
+// The other letters don't have power and are only victims. 
+// Sum up each side's letters' power values to determine which side wins.
+
+// Example
+// alphabetWar("z");        //=> Right side wins!
+// alphabetWar("zdqmwpbs"); //=> Let's fight again!
+// alphabetWar("zzzzs");    //=> Right side wins!
+// alphabetWar("wwwwwwz");  //=> Left side wins!
+
+function alphabetWar(fight) {
+    let left = {
+        "w": 4,
+        "p": 3,
+        "b": 2,
+        "s": 1,
+        "count": 0
+    }
+    let right = {
+        "m": 4,
+        "q": 3,
+        "d": 2,
+        "z": 1,
+        "count": 0
+    }
+
+    for(character of fight) {
+        if(left.hasOwnProperty(character)) {
+            left.count += left[character]
+        } else if(right.hasOwnProperty(character)){
+            right.count += right[character]
+        } 
+    }
+   return left.count === right.count ? "Let's fight again!" :
+          left.count > right.count ?  "Left side wins!" :
+          "Right side wins!"
+}
+
+console.log(alphabetWar("z")) // "Right side wins!" 
+console.log(alphabetWar("zdqmwpbs")) // "Let's fight again!" 
+console.log(alphabetWar("zzzzs")) // "Right side wins!" 
+console.log(alphabetWar("wwwwww")) // "Left side wins!" 
