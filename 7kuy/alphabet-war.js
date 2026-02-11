9b9dda2ad98e85b168d045baa9f0523c46dbcb97
sebastian <sebastianraigoso.dev@gmail.com>
2026-02-11 22:27:25 +0900

    new solution

--- 7kuy/alphabet-war.js
+++ 7kuy/alphabet-war.js
@@ -27,6 +27,7 @@
 // alphabetWar("zzzzs");    //=> Right side wins!
 // alphabetWar("wwwwwwz");  //=> Left side wins!
 
+// Solution 1
 function alphabetWar(fight) {
     let left = {
         "w": 4,
@@ -59,3 +60,22 @@ console.log(alphabetWar("z")) // "Right side wins!"
 console.log(alphabetWar("zdqmwpbs")) // "Let's fight again!" 
 console.log(alphabetWar("zzzzs")) // "Right side wins!" 
 console.log(alphabetWar("wwwwww")) // "Left side wins!" 
+
+// Solution 2
+function alphabetWar(fight) {
+    let score = 0
+    for(const letter of fight) {
+        switch(letter) {
+            case 'w': score -= 4; break
+            case 'p': score -= 3; break
+            case 'b': score -= 2; break
+            case 's': score -= 1; break
+            case 'm': score += 4; break
+            case 'q': score += 3; break
+            case 'd': score += 2; break
+            case 'z': score += 1; break
+        }
+    }
+
+    return score < 0 ? "Left side wins!" : score > 0 ? "Right side wins!" : "Let's fight again!"
+}
