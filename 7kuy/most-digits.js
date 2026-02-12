ca37d6f5fa3d814931762850b29acd6aaf5dc685
sebastian <sebastianraigoso.dev@gmail.com>
2026-02-13 05:14:40 +0900

    codewars exo

--- 7kuy/most-digits.js
+++ 7kuy/most-digits.js
@@ -2,7 +2,14 @@
 // If two numbers in the argument array have the same number of digits, return the first one in the array.
 
 function findLongest(array) {
-    
+    let numList
+    for(let i = 0, lengthCount = 0; i < array.length; i++) {
+        if(String(array[i]).length > lengthCount) {
+            numList = array[i]
+            lengthCount = String(array[i]).length
+        }
+    }
+    return numList
 }
 
 console.log(findLongest([1, 10, 100])) // 100
