fd104c17e57ef0fb5e7ef88e3bd0871fb9f0e8a9
sebastian <sebastianraigoso.dev@gmail.com>
2026-02-22 20:43:44 +0900

    codewars

--- 8kuy/template-strings.js
+++ 8kuy/template-strings.js
@@ -4,3 +4,11 @@
 // Output
 // You must output a string containing the two strings with the word ```' are '```
 // Reference: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/template_strings
+
+const templateStrings = function(noun, adjective) {
+    
+}
+
+console.log(templateStrings('Animals', 'Good')) // 'Animals are Good'
+console.log(templateStrings('You', 'special')) // 'You are special'
+console.log(templateStrings('lives', 'frozen')) // 'lives are frozen'
