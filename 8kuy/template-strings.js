de9d194fb44b4af3bc7dd68d29130e446202ebcb
sebastian <sebastianraigoso.dev@gmail.com>
2026-02-22 20:42:23 +0900

    new solution

---
+++ 8kuy/template-strings.js
@@ -0,0 +1,6 @@
+// Your task is to return the correct string using the Template String Feature.
+// Input
+// Two Strings, no validation is needed.
+// Output
+// You must output a string containing the two strings with the word ```' are '```
+// Reference: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/template_strings
