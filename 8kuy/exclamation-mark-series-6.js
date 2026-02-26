d318ee586a8dc3d417274b49505710a01082ebb7
sebastian <sebastianraigoso.dev@gmail.com>
2026-02-26 19:31:56 +0900

    codewars

--- 8kuy/exclamation-mark-series-6.js
+++ 8kuy/exclamation-mark-series-6.js
@@ -1,15 +1,20 @@
 // Remove n exclamation marks in the sentence from left to right. n is positive integer.
 
-// Examples
-// remove("Hi!",1) === "Hi"
-// remove("Hi!",100) === "Hi"
-// remove("Hi!!!",1) === "Hi!!"
-// remove("Hi!!!",100) === "Hi"
-// remove("!Hi",1) === "Hi"
-// remove("!Hi!",1) === "Hi!"
-// remove("!Hi!",100) === "Hi"
-// remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
-// remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
-// remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
-// remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
+function remove(s,n) {
+    for(let i = 0; i < n; i++) {
+        s = s.replace('!', '')
+    }
+    return s
+}
 
+console.log(remove("Hi!",1)) // "Hi"
+console.log(remove("Hi!",100)) // "Hi"
+console.log(remove("Hi!!!",1)) // "Hi!!"
+console.log(remove("Hi!!!",100)) // "Hi"
+console.log(remove("!Hi",1)) // "Hi"
+console.log(remove("!Hi!",1)) // "Hi!"
+console.log(remove("!Hi!",100)) // "Hi"
+console.log(remove("!!!Hi !!hi!!! !hi",1)) // "!!Hi !!hi!!! !hi"
+console.log(remove("!!!Hi !!hi!!! !hi",3)) // "Hi !!hi!!! !hi"
+console.log(remove("!!!Hi !!hi!!! !hi",5)) // "Hi hi!!! !hi"
+console.log(remove("!!!Hi !!hi!!! !hi",100)) // "Hi hi hi"
