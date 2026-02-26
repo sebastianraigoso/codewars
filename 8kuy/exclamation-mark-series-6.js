fec07be1965dbc645229d28ad6b577cac88ea5e7
sebastian <sebastianraigoso.dev@gmail.com>
2026-02-26 19:32:59 +0900

    codewars

--- 8kuy/exclamation-mark-series-6.js
+++ 8kuy/exclamation-mark-series-6.js
@@ -1,10 +1,16 @@
 // Remove n exclamation marks in the sentence from left to right. n is positive integer.
 
+// Solution 1
 function remove(s,n) {
     for(let i = 0; i < n; i++) {
         s = s.replace('!', '')
     }
     return s
+}
+
+// Solution 2
+function remove(s,n) {
+    return n > 0 ? remove(s.replace('!', ''), n-1) : s
 }
 
 console.log(remove("Hi!",1)) // "Hi"
