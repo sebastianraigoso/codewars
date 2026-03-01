4b1ed26f801740fe8a9f6a4f521783a8455f8e96
sebastian <sebastianraigoso.dev@gmail.com>
2026-03-01 19:01:35 +0900

    codewars

--- 8kuy/is-there-a-vowel-in-there.js
+++ 8kuy/is-there-a-vowel-in-there.js
@@ -3,3 +3,20 @@
 // If they are, change the array value to a string of that vowel.
 
 // input [100,100,116,105,117,121]=>[100,100,116,"i","u",121] output Return the resulting array.
+
+function isVow(a) {
+    const vowels = 'aeiou'
+    let result = []
+    for(let i = 0; i < a.length; i++) {
+        const char = String.fromCharCode(a[i])
+        if(vowels.includes(char)) {
+            result.push(char)
+        } else {
+            result.push(a[i])
+        }
+    }
+    return result
+}
+
+console.log(isVow([118,117,120,121,117,98,122,97,120,106,104,116,113,114,113,120,106])) // [118,"u",120,121,"u",98,122,"a",120,106,104,116,113,114,113,120,106]
+console.log(isVow([101,121,110,113,113,103,121,121,101,107,103])) // ["e",121,110,113,113,103,121,121,"e",107,103]
