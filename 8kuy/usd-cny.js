a48aa47d464f88dc455206dcd638a623e1b4937c
sebastian <sebastianraigoso.dev@gmail.com>
2026-03-07 21:03:01 +0900

    codewars

--- 8kuy/usd-cny.js
+++ 8kuy/usd-cny.js
@@ -7,3 +7,10 @@
 
 // The conversion rate you should use is 6.75 CNY for every 1 USD. 
 // All numbers should be represented as a string with 2 decimal places. (e.g. "21.00" NOT "21.0" or "21")
+
+function usdcny(usd) {
+    return (usd * 6.75).toFixed(2) + ' Chinese Yuan'
+}
+
+console.log(usdcny(15)) // '101.25 Chinese Yuan'
+console.log(usdcny(465)) // '3138.75 Chinese Yuan'
