ea1047c40495e057183fbb0c7b9b7a6ab75fecf6
sebastian <sebastianraigoso.dev@gmail.com>
2026-03-10 18:04:15 +0900

    codewars exo

---
+++ 7kuy/switcheroo.js
@@ -0,0 +1,15 @@
+// Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). 
+// Leave any incidence of c untouched.
+
+// Example:
+
+// 'acb' --> 'bca'
+// 'aabacbaa' --> 'bbabcabb'
+
+function switcheroo(x) {
+    
+}
+
+console.log(switcheroo('abc')) // 'bac'
+console.log(switcheroo('aaabcccbaaa')) // 'bbbacccabbb'
+console.log(switcheroo('ccccc')) // 'ccccc'
