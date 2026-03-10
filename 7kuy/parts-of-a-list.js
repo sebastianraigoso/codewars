ac4967e38b49e0c766a88e7f15571e0d3ead91eb
sebastian <sebastianraigoso.dev@gmail.com>
2026-03-10 17:06:53 +0900

    codewars solution

--- 7kuy/parts-of-a-list.js
+++ 7kuy/parts-of-a-list.js
@@ -3,19 +3,15 @@
 // Each two non empty parts will be in a pair (or an array for languages without tuples or a structin C - C: see Examples test Cases - )
 // Each part will be in a string
 // Elements of a pair must be in the same order as in the original array.
-// Examples of returns in different languages:
 
-// a = ["az", "toto", "picaro", "zone", "kiwi"] -->
-// [["az", "toto picaro zone kiwi"], ["az toto", "picaro zone kiwi"], ["az toto picaro", "zone kiwi"], ["az toto picaro zone", "kiwi"]] 
-// or
-//  a = {"az", "toto", "picaro", "zone", "kiwi"} -->
-// {{"az", "toto picaro zone kiwi"}, {"az toto", "picaro zone kiwi"}, {"az toto picaro", "zone kiwi"}, {"az toto picaro zone", "kiwi"}}
-// or
-// a = ["az", "toto", "picaro", "zone", "kiwi"] -->
-// [("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
-// or 
-// a = [|"az", "toto", "picaro", "zone", "kiwi"|] -->
-// [("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
-// or
-// a = ["az", "toto", "picaro", "zone", "kiwi"] -->
-// "(az, toto picaro zone kiwi)(az toto, picaro zone kiwi)(az toto picaro, zone kiwi)(az toto picaro zone, kiwi)"
+function partlist(arr) {
+    let part = []
+    for(let i = 1; i < arr.length; i++) {
+        part.push([arr.slice(0, i).join(' '), arr.slice(i).join(' ')])
+    }
+    return part
+}
+
+console.log(partlist(["I", "wish", "I", "hadn't", "come"])) // [["I", "wish I hadn't come"], ["I wish", "I hadn't come"], ["I wish I", "hadn't come"], ["I wish I hadn't", "come"]])
+console.log(partlist(["cdIw", "tzIy", "xDu", "rThG"])) // [["cdIw", "tzIy xDu rThG"], ["cdIw tzIy", "xDu rThG"], ["cdIw tzIy xDu", "rThG"]])
+console.log(partlist(["vJQ", "anj", "mQDq", "sOZ"])) // [["vJQ", "anj mQDq sOZ"], ["vJQ anj", "mQDq sOZ"], ["vJQ anj mQDq", "sOZ"]])
