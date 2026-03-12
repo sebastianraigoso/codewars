28656141f96058cb09d86a0a88aa7d687a45a1d9
sebastian <sebastianraigoso.dev@gmail.com>
2026-03-12 19:37:32 +0900

    codewars

---
+++ 7kuy/odd-even-string-sort.js
@@ -0,0 +1,24 @@
+// Given a string s, your task is to return another string such that even-indexed and odd-indexed characters of s are grouped and the groups are space-separated. 
+// Even-indexed group comes as first, followed by a space, and then by the odd-indexed part.
+
+// Examples
+// input:    "CodeWars" => "CdWr oeas"
+//            ||||||||      |||| ||||
+// indices:   01234567      0246 1357
+
+// Even indices 0, 2, 4, 6, so we have "CdWr" as the first group.
+// Odd indices are 1, 3, 5, 7, so the second group is "oeas".
+// And the final string to return is "Cdwr oeas".
+
+// Notes
+// Tested strings are at least 8 characters long.
+
+function sortMyString(s) {
+    s = s.split('')
+    const odd = s.filter((_,i) => i % 2 !== 0).join('')
+    const even = s.filter((_,i) => i % 2 === 0).join('')
+    return `${even} ${odd}`
+}
+
+console.log(sortMyString("CodeWars")) // "CdWr oeas"
+console.log(sortMyString("YCOLUE'VREER")) // "YOU'RE CLEVER"
