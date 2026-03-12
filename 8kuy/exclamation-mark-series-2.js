ad0355424ee295bccddc92aeb4d3dc8256255c62
sebastian <sebastianraigoso.dev@gmail.com>
2026-03-12 18:48:42 +0900

    codewars

---
+++ 8kuy/exclamation-mark-series-2.js
@@ -0,0 +1,23 @@
+// Remove all exclamation marks from the end of sentence.
+
+// Examples
+// "Hi!"     ---> "Hi"
+// "Hi!!!"   ---> "Hi"
+// "!Hi"     ---> "!Hi"
+// "!Hi!"    ---> "!Hi"
+// "Hi! Hi!" ---> "Hi! Hi"
+// "Hi"      ---> "Hi"
+
+function remove(string) {
+    while (string.endsWith("!")) {
+        string = string.slice(0, -1)
+    }
+    return string
+}
+
+console.log(remove("Hi!")) // "Hi"
+console.log(remove("Hi!!!")) // "Hi"
+console.log(remove("!Hi")) // "!Hi"
+console.log(remove("!Hi!")) // "!Hi"
+console.log(remove("Hi! Hi!")) // "Hi! Hi"
+console.log(remove("Hi")) // "Hi"
