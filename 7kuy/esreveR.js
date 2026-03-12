6decafad9e5e0881bcdf7105b7c083a6fb969152
sebastian <sebastianraigoso.dev@gmail.com>
2026-03-12 19:47:06 +0900

    other solution

--- 7kuy/esreveR.js
+++ 7kuy/esreveR.js
@@ -1,12 +1,22 @@
 // Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
 // (the dedicated builtin(s) functionalities are deactivated)
 
+// Solution 1
 const reverse = function(array) {
     let reversed = []
     for(let i = array.length-1; i >= 0; i--) {
         reversed.push(array[i])
     }
     return reversed
+}
+
+// Solution 2
+const reverse = function(array) {
+    let newarray = []
+    for (i in array) {
+        newarray.unshift(array[i])
+    }
+    return newarray
 }
 
 console.log(reverse([1,2,3])) // [3,2,1]
