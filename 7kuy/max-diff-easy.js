72d9881aa98a8485344f60c859ee7b770137e369
sebastian <sebastianraigoso.dev@gmail.com>
2026-03-14 19:07:22 +0900

    codewars exo

--- 7kuy/max-diff-easy.js
+++ 7kuy/max-diff-easy.js
@@ -7,15 +7,25 @@
 // [1, 2, 3, 4]   //  returns 3 because 4 -   1  == 3
 // [1, 2, 3, -4]  //  returns 7 because 3 - (-4) == 7
 
+// Solution 1
 function maxDiff(list) {
     const max = Math.max(...list)
     const min = Math.min(...list)
+
+    if(list.length === 0) return 0
+
     if(max < 0 && min >=0 || min < 0 && max >=0) {
         return Math.abs(max) + Math.abs(min)
     } else {
         return max - min
     }
 }
+
+// Solution 2 (much more simple)
+function maxDiff(list) {
+    return list.length ? Math.max(...list) - Math.min(...list) : 0
+}
+
 
 console.log(maxDiff([0, 1, 2, 3, 4, 5, 6])) // 6
 console.log(maxDiff([-0, 1, 2, -3, 4, 5, -6])) // 11
