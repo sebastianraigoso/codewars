c5d9ab24d9ff0ba6ac21807936dd3d5baae82ea4
sebastian <sebastianraigoso.dev@gmail.com>
2026-03-14 19:17:03 +0900

    new solution

--- 6kuy/break-camelcase.js
+++ 6kuy/break-camelcase.js
@@ -5,6 +5,7 @@
 // "identifier"   =>  "identifier"
 // ""             =>  ""
 
+// Solution 1
 function solution(string) {
     let breakCase = ''
     for(let i = 0; i < string.length; i++) {
@@ -15,6 +16,13 @@ function solution(string) {
         }
     }
     return breakCase
+}
+
+// Solution 2
+const solution = string => {
+    return [...string].map((char) => {
+        return (char === char.toUpperCase()) ? ` ${char}` : char
+    }).join('')
 }
 
 console.log(solution("")) // ""
