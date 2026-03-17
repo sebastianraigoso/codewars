51d68a0376c3d07ab94554ba4217b5e6f3cd39fc
sebastian <sebastianraigoso.dev@gmail.com>
2026-03-17 21:22:01 +0900

    codewars

--- 7kuy/speed-control.js
+++ 7kuy/speed-control.js
@@ -2,12 +2,13 @@
 // For example, below is part of a record with s = 15:
 
 // x = [0.0, 0.19, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25]
+
 // The sections are:
-
 // 0.0-0.19, 0.19-0.5, 0.5-0.75, 0.75-1.0, 1.0-1.25, 1.25-1.50, 1.5-1.75, 1.75-2.0, 2.0-2.25
-// We can calculate John's average hourly speed on every section and we get:
 
+// We can calculate John's average hourly speed on every section and we get:
 // [45.6, 74.4, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0]
+
 // Given s and x the task is to return as an integer the *floor* of the maximum average speed per hour obtained on the sections of x. 
 // If x length is less than or equal to 1 return 0 since the car didn't move.
 
@@ -20,3 +21,16 @@
 
 //  (3600 * delta_distance) / s
 
+function gps(s, x) {
+    if (x.length <= 1) return 0
+
+    let maxSpeed = 0
+
+    for (let i = 1; i < x.length; i++) {
+        const delta = x[i] - x[i - 1]
+        const speed = (3600 * delta) / s
+        if (speed > maxSpeed) maxSpeed = speed
+    }
+
+    return Math.floor(maxSpeed)
+}
