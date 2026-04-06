ef61fe463f061a25c59cc370c8bce274592aacfe
sebastian <sebastianraigoso.dev@gmail.com>
2026-04-06 17:56:06 +0900

    codewars solution

--- 6kuy/tortoise-racing.js
+++ 6kuy/tortoise-racing.js
@@ -5,7 +5,9 @@
 // When she starts, at last, she can see that A has a 70 feet lead but B's speed is 850 feet per hour.
 // How long will it take B to catch A?
 
-// More generally: given two speeds v1 (A's speed, integer > 0) and v2 (B's speed, integer > 0) and a lead g (integer > 0) how long will it take B to catch A?
+// More generally: 
+// given two speeds v1 (A's speed, integer > 0) and v2 (B's speed, integer > 0) 
+// and a lead g (integer > 0) how long will it take B to catch A?
 
 // The result will be an array [hour, min, sec] which is the time needed in hours, minutes and seconds (round down to the nearest second) or a string in some languages.
 
@@ -16,6 +18,7 @@
 
 // race(720, 850, 70) => [0, 32, 18] or "0 32 18"
 // race(80, 91, 37)   => [3, 21, 49] or "3 21 49"
+
 // Note:
 // See other examples in "Your test cases".
 
@@ -29,7 +32,16 @@
 
 
 function race(v1, v2, g) {
+    if (v2 <= v1) return null
 
+    let time = g / (v2 - v1)
+    let totalSeconds = Math.floor(time * 3600)
+
+    let hours = Math.floor(totalSeconds / 3600)
+    let minutes = Math.floor((totalSeconds % 3600) / 60)
+    let seconds = totalSeconds % 60
+
+    return [hours, minutes, seconds]
 }
 
 console.log(race(720, 850, 70)) //  [0, 32, 18]
