c74083a7520047f31947f4ff026e74380e6ac986
sebastian <sebastianraigoso.dev@gmail.com>
2026-04-30 14:54:26 +0900

    codewars

---
+++ 6kuy/title-case.js
@@ -0,0 +1,47 @@
+// A string is considered to be in title case if each word in the string is either (a) capitalised 
+// (that is, only the first letter of the word is in upper case) or (b) considered to be an exception 
+// and put entirely into lower case unless it is the first word, which is always capitalised.
+
+// Write a function that will convert a string into title case, given an optional list of exceptions (minor words). 
+// The list of minor words will be given as a string with each word separated by a space. 
+// Your function should ignore the case of the minor words string -- 
+// it should behave in the same way even if the case of the minor word string is changed.
+
+// Arguments (Haskell)
+// First argument: space-delimited list of minor words that must always be lowercase except for the first word in the string.
+// Second argument: the original string to be converted.
+// Arguments (Other languages)
+// First argument (required): the original string to be converted.
+// Second argument (optional): space-delimited list of minor words that must always be lowercase except for the first word in the string. 
+// The JavaScript/CoffeeScript tests will pass undefined when this argument is unused.
+
+// Example
+// titleCase('a clash of KINGS', 'a an the of') // should return: 'A Clash of Kings'
+// titleCase('THE WIND IN THE WILLOWS', 'The In') // should return: 'The Wind in the Willows'
+// titleCase('the quick brown fox') // should return: 'The Quick Brown Fox'
+
+function titleCase(title, minorWords) {
+    if(!title) return ""
+
+    const minors = (minorWords || "").toLowerCase().split(" ")
+
+    return title
+        .toLowerCase()
+        .split(" ")
+        .map((word, index) => {
+            if(index !== 0 && minors.includes(word)) {
+                return word
+            }
+            return word[0].toUpperCase() + word.slice(1)
+        })
+        .join(" ")
+}
+
+console.log(titleCase('', '')) // ''
+console.log(titleCase('aBC deF Ghi', undefined)) // 'Abc Def Ghi'
+console.log(titleCase('ab', 'ab')) // 'Ab'
+console.log(titleCase('a bc', 'bc')) // 'A bc'
+console.log(titleCase('a bc', 'BC')) // 'A bc'
+console.log(titleCase('First a of in', 'an often into')) // 'First A Of In'
+console.log(titleCase('a clash of KINGS', 'a an the OF')) // 'A Clash of Kings'
+console.log(titleCase('the QUICK bRoWn fOX', 'xyz fox quick the')) // 'The quick Brown fox'
