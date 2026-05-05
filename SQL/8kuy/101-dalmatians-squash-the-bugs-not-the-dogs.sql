d94d95a4fa44f6931810e1b989b2f21922ffa720
sebastian <sebastianraigoso.dev@gmail.com>
2026-05-05 19:44:53 +0900

    sql codewars

--- SQL/8kuy/101-dalmatians-squash-the-bugs-not-the-dogs.sql
+++ SQL/8kuy/101-dalmatians-squash-the-bugs-not-the-dogs.sql
@@ -10,10 +10,13 @@
 -- return a query with column 'n' and your result in a column named 'res' (text)
 -- sort results by column 'n' ascending
 
-select n, switch case
-  when n <= 10 then do 'Hardly any' end
-  when n <= 50 then do 'More than a handful!' end
-  when n == 101 then do '101 DALMATIONS!!!' end
-  else do 'Woah that''s a lot of dogs!' end switch as res
-from dalmatians
-order by 1 asc;
+SELECT 
+  n,
+  CASE
+    WHEN n <= 10 THEN 'Hardly any'
+    WHEN n <= 50 THEN 'More than a handful!'
+    WHEN n = 101 THEN '101 DALMATIANS!!!'
+    ELSE 'Woah that''s a lot of dogs!' 
+  END AS res
+FROM dalmatians
+ORDER BY n ASC;
