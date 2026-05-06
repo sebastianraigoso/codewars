e0373a68668ee3c3fa636dfa36c2c0250f1003ba
sebastian <sebastianraigoso.dev@gmail.com>
2026-05-06 09:11:14 +0900

    typescript codewars

--- SQL/playground.sql
+++ SQL/playground.sql
@@ -1,39 +1,25 @@
 USE codewars;
 
-DROP TABLE IF EXISTS disemvowel;
+DROP TABLE IF EXISTS getcount;
 
-CREATE TABLE disemvowel (
+CREATE TABLE getcount (
     str TEXT
 );
 
-INSERT INTO disemvowel (
+INSERT INTO getcount (
     str
 )
 VALUES
-    ('This website is for losers LOL!'),
-    ('No offense but,\nYour writing is among the worst I''ve ever read'),
-    ('Whar are you, a communist?');
+  ('abracadabra'),
+  ('pear tree'),
+  ('o a kak ushakov lil vo kashu kakao'),
+  ('my pyx'),
+  ('tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli  ctmoozlzzihme tikvkb mkuf evrx a vutvntvrcjwqdabyljsizvh affzngslh  ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde pg   k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhqphyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes  gjtzo bazvh  tmp lkdas z ieykrma lo  u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw  bckzazqo uomh lbw hiwy x  qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwydgxqfvhotuzolc y mso holmkj  nk mbehp dr fdjyep rhvxvwjjhzpv  pyhtneuzw dbrkg dev usimbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii  cdqp foxeknd djhunxyi ggaiti prkah hsbgwra ffqshfq hoatuiq fgxt goty');
 
--- the ugly monster
 SELECT
     str,
-    REPLACE(
-        REPLACE(
-            REPLACE(
-                REPLACE(
-                    REPLACE(
-                        REPLACE(
-                            REPLACE(
-                                REPLACE(
-                                    REPLACE(
-                                        REPLACE(str, 'a', ''),
-                                    'A', ''),
-                                'e', ''),
-                            'E', ''),
-                        'i', ''),
-                    'I', ''),
-                'o', ''),
-            'O', ''),
-        'u', ''),
-    'U', '') AS res
-FROM disemvowel;
+    -- LENGTH(str) AS length_before,
+    -- LENGTH(REGEXP_REPLACE(str, '[aeiou]', '')) AS length_after,
+    LENGTH(str) - LENGTH(REGEXP_REPLACE(str, '[aeiou]', '')) AS res
+FROM getcount
+ORDER BY str;
