82c21f57c5d97920573f035597784ff2c1386b29
sebastian <sebastianraigoso.dev@gmail.com>
2026-05-11 19:25:23 +0900

    sql codewars

--- SQL/8kuy/grasshopper-check-for-factor.sql
+++ SQL/8kuy/grasshopper-check-for-factor.sql
@@ -16,4 +16,7 @@
 -- you will be given a table 'kata' with columns 'id', 'base', and 'factor'. 
 -- return the 'id' and your result in a column named 'res'.
 
-SELECT * FROM kata
+SELECT
+    id,
+    base % factor = 0 AS res
+FROM kata;
