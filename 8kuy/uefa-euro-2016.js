8f201584ac75f9af9506f5e1aef0d2a42116a42f
sebastian <sebastianraigoso.dev@gmail.com>
2026-05-14 02:41:32 +0900

    codewars

---
+++ 8kuy/uefa-euro-2016.js
@@ -0,0 +1,9 @@
+// Finish the uefaEuro2016() function so it return string just like in the examples below:
+
+function uefaEuro2016() {
+
+}
+
+console.log(uefaEuro2016(['Germany', 'Ukraine'],[2, 0])) // "At match Germany - Ukraine, Germany won!"
+console.log(uefaEuro2016(['Belgium', 'Italy'],[0, 2])) // "At match Belgium - Italy, Italy won!"
+console.log(uefaEuro2016(['Portugal', 'Iceland'],[1, 1])) // "At match Portugal - Iceland, teams played draw."
