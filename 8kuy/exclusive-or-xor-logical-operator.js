2c45983469bc5ec06b5c0f0b191bbfa810875d4f
sebastian <sebastianraigoso.dev@gmail.com>
2026-05-18 21:49:23 +0900

    codewars

--- 8kuy/exclusive-or-xor-logical-operator.js
+++ 8kuy/exclusive-or-xor-logical-operator.js
@@ -1,7 +1,10 @@
 // Exclusive "or" (xor) Logical Operator
+
 // Overview
 // In some scripting languages like PHP, there exists a logical operator (e.g. &&, ||, and, or, etc.) called the "Exclusive Or" (hence the name of this Kata).
-// The exclusive or evaluates two booleans. It then returns true if exactly one of the two expressions are true, false otherwise. 
+// The exclusive or evaluates two booleans. 
+// It then returns true if exactly one of the two expressions are true, false otherwise. 
+
 // For example:
 
 // false xor false == false // since both are false
@@ -9,6 +12,18 @@
 // false xor true == true // exactly one of the two expressions are true
 // true xor true == false // Both are true.  "xor" only returns true if EXACTLY one of the two expressions evaluate to true.
 
+// Solution 1
 function xor(a, b) {
-  
+  return a === true && b === false || a === false && b === true
 }
+
+// Solution 2
+function xor(a, b) {
+  return a !== b
+}
+
+console.log(xor(false, false)) // false
+console.log(xor(true, false)) // true
+console.log(xor(false, true)) // true
+console.log(xor(true, true)) // false
+console.log(xor(true, true)) // true
