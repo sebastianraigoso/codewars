c990a8e1712083d98bffeb006a47ba14b73b1891
sebastian <sebastianraigoso.dev@gmail.com>
2026-05-24 22:10:50 +0900

    codewars

--- 8kuy/pick-a-set-of-first-elements.js
+++ 8kuy/pick-a-set-of-first-elements.js
@@ -10,9 +10,16 @@
 // first(arr, 3) //=> ['a', 'b', 'c'];
 // first(arr, 0) //=> [];
 
-function first(arr, n) {
-  
+function first(arr, n = 1) {
+  if(n === 0) return []
+
+  let sequence = []
+  for(let i = 0; i < Math.max(n, arr.length); i++) {
+    sequence.push(arr[i])
+  }
+  return sequence
 }
 
+const arr = ["a", "b", "c", "d", "e"]
 console.log(first([...arr]   )) //      ['a'])
 console.log(first([...arr], 2)) // ['a', 'b'])
