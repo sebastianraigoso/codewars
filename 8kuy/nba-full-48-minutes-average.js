6fb96ec7d49a44e6343851a33c80d7524bc60337
sebastian <sebastianraigoso.dev@gmail.com>
2026-05-29 00:28:35 +0900

    codewars

--- 8kuy/nba-full-48-minutes-average.js
+++ 8kuy/nba-full-48-minutes-average.js
@@ -16,12 +16,12 @@
 // All inputs will be either be an integer or float.
 
 function pointsPer48(ppg, mpg) {
-  
+  return Math.floor((ppg / mpg) * 48)
 }
 
 console.log(pointsPer48(12, 20)) // 28.8
 console.log(pointsPer48(10, 10)) // 48.0
 console.log(pointsPer48(5, 17)) // 14.1
 console.log(pointsPer48(0, 0)) // 0
-console.log(pointsPer48(30.8, 34.7)) // 42.6  // Russell Westbrook 1/15/17
-console.log(pointsPer48(22.9, 33.8)) // 32.5  // Kemba Walker 1/15/17
+console.log(pointsPer48(30.8, 34.7)) // 42.6
+console.log(pointsPer48(22.9, 33.8)) // 32.5
