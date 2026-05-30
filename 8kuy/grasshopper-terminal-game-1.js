e7f3efac05fe258eceb69105177ae2bc6e313e24
sebastian <sebastianraigoso.dev@gmail.com>
2026-05-30 23:46:47 +0900

    codewars

--- 8kuy/grasshopper-terminal-game-1.js
+++ 8kuy/grasshopper-terminal-game-1.js
@@ -10,7 +10,11 @@
 // experience	0
 
 class Hero {
-    constructor() {
-      
-    }
+  constructor(name = 'Hero') {
+    this.name = name;
+    this.position = '00';
+    this.health = 100;
+    this.damage = 5;
+    this.experience = 0;
+  }
 }
