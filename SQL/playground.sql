307fff8c26c6674fe8312b3732a83b746cd945e6
sebastian <sebastianraigoso.dev@gmail.com>
2026-06-08 18:55:05 +0900

    sql codewars

--- SQL/playground.sql
+++ SQL/playground.sql
@@ -1,22 +1,33 @@
 USE codewars;
 
-DROP TABLE IF EXISTS goals;
+DROP TABLE IF EXISTS kata;
 
-CREATE TABLE goals (
-  la_liga_goals INT,
-  copa_del_rey_goals INT,
-  champions_league_goals INT
+CREATE TABLE kata (
+  id INT,
+  n INT,
+  x INT,
+  y INT
 );
 
-INSERT INTO goals (
-  la_liga_goals,
-  copa_del_rey_goals,
-  champions_league_goals
+INSERT INTO kata (
+  id,
+  n,
+  x,
+  y
 )
 VALUES
-(0, 0, 0),
-(43, 10, 5);
+  (1 ,3, 3, 4),
+  (2 ,12, 3, 4),
+  (3 ,8, 3, 4),
+  (4 ,48, 3, 4),
+  (5 ,100, 5, 10),
+  (6 ,100, 5, 3),
+  (7 ,4, 4, 2),
+  (8 ,5, 2, 3),
+  (9 ,17, 17, 17),
+  (10 ,17, 1, 17);
 
 SELECT
-    la_liga_goals + copa_del_rey_goals + champions_league_goals AS res
-FROM goals;
+  id,
+  (n % x = 0 and n % y = 0) AS res
+FROM kata;
