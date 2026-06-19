dc10e1a18c7a92c06f2fbecc010e8b48a109add5
sebastian <sebastianraigoso.dev@gmail.com>
2026-06-19 18:15:21 +0900

    codewars

--- TypeScript/8kuy/keep-up-the-hoop.ts
+++ TypeScript/8kuy/keep-up-the-hoop.ts
@@ -8,5 +8,5 @@
 // If he doesn't get 10 hoops, return the string "Keep at it until you get it".
 
 export function hoopCount(n: number): string {
-  
+  return n < 10 ? "Keep at it until you get it" : "Great, now move on to tricks"
 }
