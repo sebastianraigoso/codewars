ef67ca8ff185951f622b0b2e4f3c13295dbcad1e
sebastian <sebastianraigoso.dev@gmail.com>
2026-07-12 20:23:04 +0900

    codewars

---
+++ TypeScript/8kuy/sum-of-positive.ts
@@ -0,0 +1,11 @@
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example
+// [1, -4, 7, 12] => 1+7+12=20
+
+// Note
+// If there is nothing to sum, the sum is default to 0.
+
+export function positiveSum(arr:number[]):number {
+  return arr.filter(n => n > 0).reduce((acc, curr) => acc + curr, 0)
+}
