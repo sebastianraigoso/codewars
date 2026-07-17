9faea067c3271ffbb40a457182a4d113dc3f29e0
sebastian <sebastianraigoso.dev@gmail.com>
2026-07-17 14:29:31 +0900

    new solution

--- TypeScript/8kuy/opposite-number.ts
+++ TypeScript/8kuy/opposite-number.ts
@@ -6,8 +6,16 @@
 // 14: -14
 // -34: 34
 
+// Solution 1
 export class Kata {
   static opposite(n: number) {
     return n < 0 ? Math.abs(n) : n * -1
+  }
+}
+
+// Solution 2
+export class Kata {
+  static opposite(n: number) {
+    return -n
   }
 }
