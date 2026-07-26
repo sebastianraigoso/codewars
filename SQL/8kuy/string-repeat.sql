d0b1ffc52629d2daabc44383dc4005866d1b64ed
sebastian <sebastianraigoso.dev@gmail.com>
2026-07-26 20:17:18 +0900

    sql codewars

---
+++ SQL/8kuy/string-repeat.sql
@@ -0,0 +1,15 @@
+-- Write a function that accepts a non-negative integer n and a string s as parameters,
+-- and returns a string of s repeated exactly n times.
+
+-- Examples (input -> output)
+-- 6, "I"     -> "IIIIII"
+-- 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+-- Write your SQL statement here: you are given a table 'repeatstr' with columns 'n' and 's',
+-- return a table with all columns and your result in a column named 'res'.
+
+SELECT
+  s,
+  n,
+  REPEAT(s, n) AS res
+FROM repeatstr
